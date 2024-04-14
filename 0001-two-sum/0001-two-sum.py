c666bb9aed0faef9daa2415a1d7046d0e065943b
UnMoutonPerdu <mtnhugo062@gmail.com>
2024-04-14 11:18:25 -0400

    clean and start using LeetSync instead of LeetHub

--- 0001-two-sum/0001-two-sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        diff = {}
-        for i in range(len(nums)):
-            if nums[i] in diff:
-                return [i, diff[nums[i]]]
-            diff[target - nums[i]] = i
--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        runSum = []
-        previous = 0
-        for elem in nums:
-            runSum.append(elem + previous)
-            previous = runSum[-1]
-
-        return runSum 
